8c13269a88fa9cd9a303a8bb37f71cf5410fd71a
ATLURI VINOD <atlurivinod406@gmail.com>
2026-07-04 00:07:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load Trained Model
+model = pickle.load(open("model.pkl", "rb"))
+
+# Home Page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# Prediction Form Page
+@app.route("/predict_page")
+def predict_page():
+    return render_template("index.html")
+
+# Prediction
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    features = [
+        float(request.form["ID"]),
+        float(request.form["CODE_GENDER"]),
+        float(request.form["FLAG_OWN_CAR"]),
+        float(request.form["FLAG_OWN_REALTY"]),
+        float(request.form["CNT_CHILDREN"]),
+        float(request.form["AMT_INCOME_TOTAL"]),
+        float(request.form["NAME_INCOME_TYPE"]),
+        float(request.form["NAME_EDUCATION_TYPE"]),
+        float(request.form["NAME_FAMILY_STATUS"]),
+        float(request.form["NAME_HOUSING_TYPE"]),
+        float(request.form["DAYS_BIRTH"]),
+        float(request.form["DAYS_EMPLOYED"]),
+        float(request.form["FLAG_MOBIL"]),
+        float(request.form["FLAG_WORK_PHONE"]),
+        float(request.form["FLAG_PHONE"]),
+        float(request.form["FLAG_EMAIL"]),
+        float(request.form["OCCUPATION_TYPE"]),
+        float(request.form["CNT_FAM_MEMBERS"]),
+        float(request.form["MONTHS_BALANCE"])
+    ]
+
+    prediction = model.predict([features])
+
+    if prediction[0] == 0:
+        result = "Credit Card Approved"
+    else:
+        result = "Credit Card Rejected"
+
+    return render_template("result.html", prediction=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
